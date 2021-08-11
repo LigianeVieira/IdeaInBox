03845645b7f8268ce057552ad754ef7fb66ab20b
Miguel Santiago <miguelsantiago1812@gmail.com>
2021-08-11 17:18:36 -0300

    Criado a classe de Administrador
    Co-Authored-By: Ligiane Vieira <87396681+LigianeVieira@users.noreply.github.com>
    Co-Authored-By: Davi Saviano Moraes <87396299+Davi1234567891099@users.noreply.github.com>
    Co-Authored-By: Gustavo Guirado dos Santos <87440214+GustavoGuirado@users.noreply.github.com>

---
+++ src/main/java/com/IdeaBox/models/usuarios/Administrador.java
@@ -0,0 +1,16 @@
+package com.IdeaBox.models.usuarios;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Administrador extends Usuario {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Administrador() {
+		
+	}
+}
