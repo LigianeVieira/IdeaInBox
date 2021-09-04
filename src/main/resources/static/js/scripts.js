5976ff77792b725eb78007042166047ec58c16a4
Ligiane Vieira <ligiane_vieira@msn.com>
2021-09-03 22:07:42 -0300

    Criado e alterado pag Html e Css /  JS ano footer Okay

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,41 +1,62 @@
-//footer ano
-window.jQuery(document).ready(function($){
-    let ano = new Date().getFullYear();
-    $('.ano').text(ano);
+//footer ano funcionando!
+
+ document.getElementById("ano").innerHTML = new Date().getFullYear();
+
+
+
+
+//senha eye testar
+var input = document.querySelector("#password");
+var img = document.querySelector('#lnr-eye');
+var visivel = false;
+img.addEventListener('mousedown', function () {
+  visivel = true;
+  input.type = 'text';
+});
+window.addEventListener('mouseup', function () {
+  if (visivel) visivel = !visivel;
+  input.type = 'password';
 });
 
-// Validações formulario
 
-//cpf
+
+
+
+//ou senha eye testar
+
+/*let btn = document.querySelector('.lnr lnr-eye');
+
+  btn.addEventListener('click', function() {
+
+    let input = document.querySelector('.password');
+
+    if(input.getAttribute('type' == 'password')) {
+        input.setAttribute('type', 'text');
+    } else {
+        input.setAttribute('type', 'password');}
+ });*/
+
+ //Validações formulario
+
+//cpf erro
+
+
 
 function validacaoCpf(cpf) {
-	
-let cpf = document.getElementById("cpf").value;
+var cpf = document.getElementById("cpf").value;
  cpf = cpf.replace(/[\s.-]*/igm, '')
-
-    if (typeof cpf !== "string") return false
    
-    if (
-        !cpf ||
-        cpf.length != 11 ||
-        cpf == "00000000000" ||
-        cpf == "11111111111" ||
-        cpf == "22222222222" ||
-        cpf == "33333333333" ||
-        cpf == "44444444444" ||
-        cpf == "55555555555" ||
-        cpf == "66666666666" ||
-        cpf == "77777777777" ||
-        cpf == "88888888888" ||
-        cpf == "99999999999" 
+    if (!cpf || cpf.length != 11 ||cpf == "00000000000" ||cpf == "11111111111" ||
+        cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" ||
+        cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" ||
+        cpf == "88888888888" || cpf == "99999999999" 
     ) 
         return false
-    
     var soma = 0
     var resto
     for (var i = 1; i <= 9; i++) 
         soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
-    resto = (soma * 10) % 11
+      resto = (soma * 10) % 11
     if ((resto == 10) || (resto == 11))  resto = 0
     if (resto != parseInt(cpf.substring(9, 10)) ) return false
     soma = 0
@@ -45,7 +66,7 @@ let cpf = document.getElementById("cpf").value;
     if ((resto == 10) || (resto == 11))  resto = 0
     if (resto != parseInt(cpf.substring(10, 11) ) ) return false
     return true
-}
+
 
 
 //e-mail
@@ -72,4 +93,4 @@ document.getElementById("msgemail").innerHTML="<font color='red'>Email inválido
 alert("E-mail invalido");
 }
 }*/
-
+}
