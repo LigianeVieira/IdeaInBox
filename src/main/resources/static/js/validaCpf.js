1efbf5b956c7e3e3e1f47918f7db0a395ee093e1
Ligiane Vieira <ligiane_vieira@msn.com>
2021-09-05 22:06:47 -0300

    Validações formulario/Cpf

---
+++ src/main/resources/static/js/validaCpf.js
@@ -0,0 +1,74 @@
+class ValidaCPF {
+    constructor(cpfEnviado) {
+      Object.defineProperty(this, 'cpfLimpo', {
+        writable: false,
+        enumerable: true,
+        configurable: false,
+        value: cpfEnviado.replace(/\D+/g, '')
+      });
+    }
+  
+    éSequência() {
+      return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+  
+    geraNovoCpf() {
+      const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+      const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+      const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+      this.novoCPF = cpfSemDigitos + digito1 + digito2;
+    }
+  
+    static geraDigito(cpfSemDigitos) {
+      let total = 0;
+      let reverso = cpfSemDigitos.length + 1;
+  
+      for(let stringNumerica of cpfSemDigitos) {
+        total += reverso * Number(stringNumerica);
+        reverso--;
+      }
+  
+      const digito = 11 - (total % 11);
+      return digito <= 9 ? String(digito) : '0';
+    }
+  
+    valida() {
+      if(!this.cpfLimpo) return false;
+      if(typeof this.cpfLimpo !== 'string') return false;
+      if(this.cpfLimpo.length !== 11) return false;
+      if(this.éSequência()) return false;
+      this.geraNovoCpf();
+  
+      return this.novoCPF === this.cpfLimpo;
+    }
+  }
+
+
+  /*function validacaoCpf() {
+    var cpf = document.getElementById("cpf").value;
+     cpf = cpf.replace(/[\s.-]*////
+        ///////////////////igm, '')*/
+    
+     /*if(typeof cpf=="undefined") return false;
+     if(cpf.length!=11)return false;
+     if (cpf == "00000000000" ||cpf == "11111111111" ||
+            cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" ||
+            cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" ||
+            cpf == "88888888888" || cpf == "99999999999" 
+        ) return false
+        var soma = 0
+        var resto
+        for (var i = 1; i <= 9; i++) 
+          soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i)
+          resto = (soma * 10) % 11
+        if ((resto == 10) || (resto == 11))  resto = 0
+        if (resto != parseInt(cpf.substring(9, 10)) ) return false
+        soma = 0
+        for (var i = 1; i <= 10; i++) 
+            soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i)
+        resto = (soma * 10) % 11
+        if ((resto == 10) || (resto == 11))  resto = 0
+        if (resto != parseInt(cpf.substring(10, 11) ) ) return false
+        return true*/
+    
+    
