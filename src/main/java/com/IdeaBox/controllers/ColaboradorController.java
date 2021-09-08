2961a67201a17067b5a359fa2ab33734cbc08616
Miguel Santiago <miguelsantiago1812@gmail.com>
2021-09-08 15:21:53 -0300

    ✔Atualizado nome

--- src/main/java/com/IdeaBox/controllers/ColaboradorController.java
+++ src/main/java/com/IdeaBox/controllers/ColaboradorController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.IdeaBox.models.sugestoes.Sugestao;
@@ -46,5 +48,13 @@ public class ColaboradorController {
 		cr.delete(colaborador);
 		return "redirect:/colaboradores";
 	}
+	
+	@PostMapping("/editarNome")
+	 public String editarColaborador(@RequestParam long id, @RequestParam("nome") String nome) {
+			Colaborador colaborador = cr.findById(id);
+			colaborador.setNome(nome);
+			cr.save(colaborador);
+				return "redirect:/colaboradores";
+			}
 
 }
